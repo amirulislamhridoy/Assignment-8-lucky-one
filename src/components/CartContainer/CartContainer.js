281298c55ef2476090505a4f9cef95e045cbd7dd
amirulislamhridoy <hossainhridoy30687@gmail.com>
2022-03-26 02:38:13 +0600

    add-button-fontawesome

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,5 +1,8 @@
+import ReactDOM from 'react-dom'
 import React from 'react';
 import './CartContainer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const CartContainer = (props) => {
     // console.log(props)
@@ -9,7 +12,10 @@ const CartContainer = (props) => {
             <div className='cart-info'>
                 <h3>{props.tv.name}</h3>
                 <h1>${props.tv.price}</h1>
-                <button className='cart-btn'>Add to Choice List</button>
+                <button className='cart-btn'>
+                    Add to Choice List
+                    <FontAwesomeIcon className='icon' icon={faCartShopping} spin></FontAwesomeIcon>
+                </button>
             </div>
         </div>
     );
