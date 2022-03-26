d7cb8a672a1c4d2b8c9189e88d2d95a569885247
amirulislamhridoy <hossainhridoy30687@gmail.com>
2022-03-26 16:46:29 +0600

    change url

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ function App() {
     <div>
       <Header></Header>
       <Main></Main>
+      <h2>How to react works?</h2>
+      <p>React is a declarative code.</p>
+      <div>
+        <h2>useState vs props</h2>
+        <p>1) useState may change. And props don't may change. </p>
+        <p>2) useState can not change his place. if we want send any component or place, we can send by props</p>
+      </div>
     </div>
   );
 }
